77c7a3e33b8b3aa7bab76a0284897a8e81f27ee7
mengchaoheng <1215983914@qq.com>
2020-08-31 15:05:00 +0800

    first commit

---
+++ all.bat
@@ -0,0 +1,7 @@
+del *.aux *.lo? *.toc *.ind *.inx *.gls *.glo *.ist *.idx *.ilg *.out *.bak *.bbl *.brf *.blg *.dvi *.ps *.xdv  *.aux
+del scutthesis.pdf
+
+xelatex -no-pdf --interaction=nonstopmode scutthesis
+biber scutthesis
+xelatex -no-pdf --interaction=nonstopmode scutthesis
+xelatex --interaction=nonstopmode scutthesis
