1b8f009447ff5c08b081bfff5501212cbafc426a
mengchaoheng <chaohengmeng@163.com>
2022-02-25 20:51:11 +0800

    remove some file

--- all.bat
+++ all.bat
@@ -1,4 +1,4 @@
-del *.aux *.lo? *.toc *.ind *.inx *.gls *.glo *.ist *.idx *.ilg *.out *.bak *.bbl *.brf *.blg *.dvi *.ps *.xdv  *.aux
+del *.aux *.lo? *.toc *.ind *.inx *.gls *.glo *.ist *.idx *.ilg *.out *.bak *.bbl *.brf *.blg *.dvi *.ps *.xdv  *.aux *.bcf *.run.xml *.synctex.gz
 del scutthesis.pdf
 
 xelatex -no-pdf --interaction=nonstopmode scutthesis
