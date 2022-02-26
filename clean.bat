eccb2f0517015780c77fd5754b61818c8e5b00a2
mengchaoheng <chaohengmeng@163.com>
2022-02-26 18:53:38 +0800

    fix tocbibind bug

--- clean.bat
+++ clean.bat
@@ -1,2 +1,1 @@
 del *.aux *.lo? *.toc *.ind *.inx *.gls *.glo *.ist *.idx *.ilg *.out *.bak *.bbl *.brf *.blg *.dvi *.ps *.xdv  *.aux *.bcf *.run.xml *.synctex.gz
-del scutthesis.pdf
---
+++ cover_file/readme.txt
@@ -0,0 +1,3 @@
+使用方法：
+1.根据个人信息编辑好word文档，注意添加必要的空白页，或者也可以生成pdf文件后编辑
+2.生成pdf文件，重命名为thesis_cover.pdf，再拷贝到根目录（scutthesis.tex所在目录）
